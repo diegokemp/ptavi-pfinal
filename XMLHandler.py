ad8e423957185bea98e70d1309af5b82609f1e75
diegokemp <da.sanchezm@alumnos.urjc.es>
2016-01-10 11:56:10 +0100

    subido

---
+++ XMLHandler.py
@@ -0,0 +1,39 @@
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+
+class XMLHandler(ContentHandler):
+
+    def __init__(self):
+        self.diccionario = {}
+
+    def startElement(self, etiqueta, attrs):
+
+        if etiqueta == 'account':
+            self.diccionario["username"] = attrs.get("username","")
+            self.diccionario["passwd"] = attrs.get("passwd","")
+        elif etiqueta == 'uaserver':
+            self.diccionario["uasip"] = attrs.get("ip","")
+            self.diccionario["uasport"] = attrs.get("puerto","")
+        elif etiqueta == 'rtpaudio':
+            self.diccionario["rtport"] = attrs.get("puerto","")
+        elif etiqueta == 'regproxy':
+            self.diccionario["proxyip"] = attrs.get("ip","")
+            self.diccionario["proxyport"] = attrs.get("puerto","")
+        elif etiqueta == 'log':
+            self.diccionario["logpath"] = attrs.get("path","")
+        elif etiqueta == 'audio':
+            self.diccionario["mp3path"] = attrs.get("path","")
+
+    def get_tags(self):
+        return self.diccionario
+
+if __name__ == "__main__":
+
+    parser = make_parser()
+    cHandler = XMLHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open("ua1.xml"))
+
+    listatotal = cHandler.get_tags()
+    print(listatotal)
